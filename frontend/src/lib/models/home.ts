9710eeacea03fde7ee9f50985d149320356541bc
Andy Jung <wontaethedev@gmail.com>
2024-11-11 06:13:38 +0900

    Update README.md for frontend, add axios package, Add networking between frontend and backend, Test networking to call dev POST request on the backend on the Home Page

---
+++ frontend/src/lib/models/home.ts
@@ -0,0 +1,4 @@
+export interface HealthData {
+  healthy: boolean;
+  message: string;
+}
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,9 +1,47 @@
-import React from "react";
-import "@/pages/home/index.scss";
+import React, { useEffect, useState } from "react";
+
 import "@/assets/styles/common-styles.scss";
+import "@/pages/home/index.scss";
+
+import { HealthData } from "@/lib/models/home";
+import { getHealth } from "@/lib/api/dev";
 
 const HomePage: React.FC = () => {
-  return <div className="home-page c-page">HOME</div>;
+  const [healthData, setHealthData] = useState<HealthData | null>(null);
+  const [isHealthDataLoading, setIsHealthDataLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsHealthDataLoading(true);
+        const result = await getHealth();
+        setHealthData(result);
+      } catch (err) {
+        setError("Failed to fetch data");
+        console.error("Error fetching data:", err);
+      } finally {
+        setIsHealthDataLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="home-page c-page">
+      {isHealthDataLoading ? (
+        <span>Loading...</span>
+      ) : error ? (
+        <span>{error}</span>
+      ) : (
+        <>
+          <span>{healthData?.healthy ? "Healthy::" : "Not Healthy::"}</span>
+          <span>{healthData?.message}</span>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
