31af83b7289ea1e5c0f8383c5405f9ee4f2ea74f
Andy Jung <wontaethedev@gmail.com>
2024-11-11 03:49:25 +0900

    Setup frontend repository

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "@/pages/home/index.scss";
+import "@/assets/styles/common-styles.scss";
+
+const HomePage: React.FC = () => {
+  return <div className="home-page c-page">HOME</div>;
+};
+
+export default HomePage;
