89d2b9e37aa2dcb95639c301b2ffca4008501f58
Andy Jung <wontaethedev@gmail.com>
2025-03-15 15:03:18 +0900

    cleanup

--- backend/app-core/app.py
+++ backend/app-core/app.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from router.dev import router as dev_router
-from router.resume import router as resume_router
-from router.role import router as role_router
 
 
 app: FastAPI = FastAPI()
@@ -23,5 +21,3 @@ app.add_middleware(
 )
 
 app.include_router(dev_router, prefix="/dev")
-app.include_router(resume_router, prefix="/resume")
-app.include_router(role_router, prefix="/role")
--- backend/app-core/lib/scripts/process_resumes.py
+++ backend/app-core/lib/scripts/process_resumes.py
@@ -1,139 +1,139 @@
-import logging
+# import logging
 
-from lib.helpers.openai import OpenAIHelper
-from lib.helpers.db.resume import ResumeDBHelper
-from lib.helpers.db.role import RoleDBHelper
-from lib.data.openai import TOOLS
-from lib.models.product.resume import RoleTypes, StatusTypes
+# from lib.helpers.openai import OpenAIHelper
+# from lib.helpers.db.resume import ResumeDBHelper
+# from lib.helpers.db.role import RoleDBHelper
+# from lib.data.openai import TOOLS
+# from lib.models.product.resume import RoleTypes, StatusTypes
 
-from db.db import get_context_managed_session
-from db.models import Resume, Role
+# from db.db import get_context_managed_session
+# from db.models import Resume, Role
 
-from settings import open_api_settings
+# from settings import open_api_settings
 
 
-async def process_resumes() -> list[str]:
-    """
-    Processes resumes of status PENDING by using OpenAI for
-    assessment based on preset data in `app-core/lib/data/openai.py`.
+# async def process_resumes() -> list[str]:
+#     """
+#     Processes resumes of status PENDING by using OpenAI for
+#     assessment based on preset data in `app-core/lib/data/openai.py`.
 
-    TODO: Add `created_at` in `Resume` model and process from oldest resume
-    TODO: Make `max_num_resumes` controllable
+#     TODO: Add `created_at` in `Resume` model and process from oldest resume
+#     TODO: Make `max_num_resumes` controllable
 
-    Returns:
-      - list[str]: The IDs of resumes that were processed
-    """
+#     Returns:
+#       - list[str]: The IDs of resumes that were processed
+#     """
 
-    processed_resume_ids: list[str] = []
+#     processed_resume_ids: list[str] = []
 
-    async with get_context_managed_session() as session:
-        resumes: list[Resume] = await ResumeDBHelper.select_by_filters(
-            session=session,
-            status=StatusTypes.PENDING,
-        )
+#     async with get_context_managed_session() as session:
+#         resumes: list[Resume] = await ResumeDBHelper.select_by_filters(
+#             session=session,
+#             status=StatusTypes.PENDING,
+#         )
 
-        resume_ids: list[str] = [resume.id for resume in resumes]
+#         resume_ids: list[str] = [resume.id for resume in resumes]
 
-        try:
-            await ResumeDBHelper.bulk_update_status(
-                session=session, ids=resume_ids, status=StatusTypes.IN_PROGRESS
-            )
-        except Exception as e:
-            await ResumeDBHelper.bulk_update_status(
-                session=session, ids=resume_ids, status=StatusTypes.FAILED
-            )
-            raise Exception(
-                f"Failed to set target resumes in status IN_PROGRESS | {str(e)}"
-            )
+#         try:
+#             await ResumeDBHelper.bulk_update_status(
+#                 session=session, ids=resume_ids, status=StatusTypes.IN_PROGRESS
+#             )
+#         except Exception as e:
+#             await ResumeDBHelper.bulk_update_status(
+#                 session=session, ids=resume_ids, status=StatusTypes.FAILED
+#             )
+#             raise Exception(
+#                 f"Failed to set target resumes in status IN_PROGRESS | {str(e)}"
+#             )
 
-        for resume in resumes:
-            resume_id: str = resume.id
-            resume_content: str = resume.content
-            resume_role_id: str = resume.role_id
+#         for resume in resumes:
+#             resume_id: str = resume.id
+#             resume_content: str = resume.content
+#             resume_role_id: str = resume.role_id
 
-            try:
-                resume_role: Role = await RoleDBHelper.get_role(
-                    session=session, id=resume_role_id
-                )
-            except Exception as e:
-                await ResumeDBHelper.update_status(
-                    session=session, id=resume_id, status=StatusTypes.FAILED
-                )
-                logging.error(
-                    f"Could not find associated role for the resume | {str(e)}"
-                )
-                continue
+#             try:
+#                 resume_role: Role = await RoleDBHelper.get_role(
+#                     session=session, id=resume_role_id
+#                 )
+#             except Exception as e:
+#                 await ResumeDBHelper.update_status(
+#                     session=session, id=resume_id, status=StatusTypes.FAILED
+#                 )
+#                 logging.error(
+#                     f"Could not find associated role for the resume | {str(e)}"
+#                 )
+#                 continue
 
-            role_description: str = resume_role.description
+#             role_description: str = resume_role.description
 
-            try:
-                openai_helper: OpenAIHelper = OpenAIHelper(
-                    api_key=open_api_settings.API_KEY,
-                    organization_id=open_api_settings.ORGANIZATION_ID,
-                )
+#             try:
+#                 openai_helper: OpenAIHelper = OpenAIHelper(
+#                     api_key=open_api_settings.API_KEY,
+#                     organization_id=open_api_settings.ORGANIZATION_ID,
+#                 )
 
-                # Process resume through OpenAI
-                resume_data = openai_helper.function_call_prompt(
-                    user_msg=f"Here is the resume text: {str(resume_content)}",
-                    system_msg=f"""
-            You are a copilot assisting a hiring manager review resumes.
-            Here is the job description:
-            {role_description}
-          """,
-                    tools=[TOOLS[RoleTypes.SENIOR_PRODUCT_ENGINEER]],
-                )
-            except Exception as e:
-                await ResumeDBHelper.update_status(
-                    session=session, id=resume_id, status=StatusTypes.FAILED
-                )
-                logging.error(f"Failed to assess resume using OpenAI | {str(e)}")
-                continue
+#                 # Process resume through OpenAI
+#                 resume_data = openai_helper.function_call_prompt(
+#                     user_msg=f"Here is the resume text: {str(resume_content)}",
+#                     system_msg=f"""
+#             You are a copilot assisting a hiring manager review resumes.
+#             Here is the job description:
+#             {role_description}
+#           """,
+#                     tools=[TOOLS[RoleTypes.SENIOR_PRODUCT_ENGINEER]],
+#                 )
+#             except Exception as e:
+#                 await ResumeDBHelper.update_status(
+#                     session=session, id=resume_id, status=StatusTypes.FAILED
+#                 )
+#                 logging.error(f"Failed to assess resume using OpenAI | {str(e)}")
+#                 continue
 
-            try:
-                base_requirement_satisfaction_score: int = resume_data[
-                    "base_requirement_satisfaction_score"
-                ]
-                exceptionals: str = resume_data["exceptionals"]
-                fitness_score: int = resume_data["fitness_score"]
-            except Exception as e:
-                await ResumeDBHelper.update_status(
-                    session=session, id=resume_id, status=StatusTypes.FAILED
-                )
-                logging.error(
-                    f"Failed to parse assessment data from OpenAI response | {str(e)}"
-                )
-                continue
+#             try:
+#                 base_requirement_satisfaction_score: int = resume_data[
+#                     "base_requirement_satisfaction_score"
+#                 ]
+#                 exceptionals: str = resume_data["exceptionals"]
+#                 fitness_score: int = resume_data["fitness_score"]
+#             except Exception as e:
+#                 await ResumeDBHelper.update_status(
+#                     session=session, id=resume_id, status=StatusTypes.FAILED
+#                 )
+#                 logging.error(
+#                     f"Failed to parse assessment data from OpenAI response | {str(e)}"
+#                 )
+#                 continue
 
-            try:
-                await ResumeDBHelper.update(
-                    session=session,
-                    id=resume_id,
-                    base_requirement_satisfaction_score=base_requirement_satisfaction_score,
-                    exceptional_considerations=exceptionals,
-                    fitness_score=fitness_score,
-                )
-            except Exception as e:
-                await ResumeDBHelper.update_status(
-                    session=session, id=resume_id, status=StatusTypes.FAILED
-                )
-                logging.error(
-                    f"Failed to update resume information using OpenAI response | {str(e)}"
-                )
-                continue
+#             try:
+#                 await ResumeDBHelper.update(
+#                     session=session,
+#                     id=resume_id,
+#                     base_requirement_satisfaction_score=base_requirement_satisfaction_score,
+#                     exceptional_considerations=exceptionals,
+#                     fitness_score=fitness_score,
+#                 )
+#             except Exception as e:
+#                 await ResumeDBHelper.update_status(
+#                     session=session, id=resume_id, status=StatusTypes.FAILED
+#                 )
+#                 logging.error(
+#                     f"Failed to update resume information using OpenAI response | {str(e)}"
+#                 )
+#                 continue
 
-            try:
-                await ResumeDBHelper.update_status(
-                    session=session, id=resume_id, status=StatusTypes.COMPLETE
-                )
-            except Exception as e:
-                await ResumeDBHelper.update_status(
-                    session=session, id=resume_id, status=StatusTypes.FAILED
-                )
-                logging.error(
-                    f"Failed to update status of new resume to COMPLETE | {str(e)}"
-                )
-                continue
+#             try:
+#                 await ResumeDBHelper.update_status(
+#                     session=session, id=resume_id, status=StatusTypes.COMPLETE
+#                 )
+#             except Exception as e:
+#                 await ResumeDBHelper.update_status(
+#                     session=session, id=resume_id, status=StatusTypes.FAILED
+#                 )
+#                 logging.error(
+#                     f"Failed to update status of new resume to COMPLETE | {str(e)}"
+#                 )
+#                 continue
 
-            processed_resume_ids.append(resume_id)
+#             processed_resume_ids.append(resume_id)
 
-        return processed_resume_ids
+#         return processed_resume_ids
